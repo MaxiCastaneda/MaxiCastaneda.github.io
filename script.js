76ffaa138b11cdc5c7722e424dc89469e7c3a24f
Maxi Castañeda <maxicastaneda@gmail.com>
2024-05-21 18:11:24 +1000

    Modificación de efecto hover sobre imagen

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => { 
-  const navToggle = document.getElementById('nav-toggle'); 
-  const navList = document.getElementById('nav-list'); 
-
-  navToggle.addEventListener('click', () => { 
-    navList.classList.toggle('show'); 
-  }); 
-});
