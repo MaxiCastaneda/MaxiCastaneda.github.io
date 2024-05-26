924e239d24ef8ec23b528e528d1364755649036a
Maxi Castañeda <maxicastaneda@gmail.com>
2024-05-26 22:51:40 +1000

    Fixed Js

--- script.js
+++ script.js
@@ -17,31 +17,48 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   };
 
+  const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+
   sections.forEach(section => {
     const target = document.querySelector(section.trigger);
     if (target) {
       target.dataset.target = section.target;
       target.dataset.className = section.className;
-      const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
       observer.observe(target);
     }
   });
+
+  // Selectores de elementos para la navegación
+  const navToggle = document.querySelector('.menu-toggle');
+  const navList = document.querySelector('.nav-list');
+  const nav = document.querySelector('.nav');
+
+  // Event listener para el toggle del menú
+  navToggle.addEventListener('click', () => {
+    navList.classList.toggle('active');
+    nav.classList.toggle('active');
+    const isExpanded = navToggle.getAttribute('aria-expanded') === 'true' || false;
+    navToggle.setAttribute('aria-expanded', !isExpanded);
+  });
+
+  // Cerrar el menú cuando se hace clic en un enlace
+  navList.addEventListener('click', () => {
+    navList.classList.remove('active');
+    nav.classList.remove('active');
+    navToggle.setAttribute('aria-expanded', 'false');
+  });
 });
 
-// Selectores de elementos
-const navToggle = document.querySelector('.menu-toggle');
-const navList = document.querySelector('.nav-list');
-const nav = document.querySelector('.nav');
-
-// Event listener para el toggle del menú
-navToggle.addEventListener('click', () => {
-  // Toggle de la clase "active" en la lista de navegación y en la barra de navegación
-  navList.classList.toggle('active');
-  nav.classList.toggle('active');
-});
-
-// Cerrar el menú cuando se hace clic en un enlace
-navList.addEventListener('click', () => {
-  navList.classList.remove('active');
-  nav.classList.remove('active');
-});
+function toggleImage() {
+  const fotoPerfil = document.querySelector('.foto-perfil');
+  const eyeButton = document.querySelector('.eye-button i');
+  if (fotoPerfil.style.filter === 'brightness(100%)') {
+    fotoPerfil.style.filter = 'brightness(10%)';
+    eyeButton.classList.remove('fa-eye-slash');
+    eyeButton.classList.add('fa-eye');
+  } else {
+    fotoPerfil.style.filter = 'brightness(100%)';
+    eyeButton.classList.remove('fa-eye');
+    eyeButton.classList.add('fa-eye-slash');
+  }
+}
